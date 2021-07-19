7ff8933c9f432c9e17f0f9cedd4f578adb88c897
Mohammad Javad Pooladkhay <mo@cfbtel.com>
2021-07-19 05:02:44 +0430

    add ability to assign sub-service-name to logs

--- example/main.go
+++ example/main.go
@@ -3,10 +3,22 @@ package main
 import "github.com/pooladkhay/mogger"
 
 func main() {
-	m := mogger.NewMogger("my_service_name", "./logs")
+	m := mogger.NewMogger("mogger_example", "./logs")
 
-	m.LogToFile(mogger.Info, "this is an info log")
-	m.LogToFile(mogger.Debug, "this is a debug log")
-	m.LogToFile(mogger.Warn, "this is a warn log")
-	m.LogToFile(mogger.Fatal, "this is a fatal log")
+	// Log without subServiceName
+	m.LogToFile(mogger.Debug, "without subServiceName")
+
+	// Log with subServiceName
+	log := m.AddSubService("Main_func")
+	log.LogToFile(mogger.Info, "this is an info log")
+	log.LogToFile(mogger.Warn, "this is a warn log")
+
+	anotherFunc(m)
+}
+
+func anotherFunc(m mogger.Mogger) {
+	// Log with subServiceName
+	log := m.AddSubService("Another_func")
+	log.LogToFile(mogger.Debug, "this is a debug log")
+	log.LogToFile(mogger.Fatal, "this is a fatal log")
 }
